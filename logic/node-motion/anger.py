baea7533bc453082b9143190f4336e601e41949f
vengadora <vengadoravg@gmail.com>
2014-04-12 18:26:52 -0400

    i'm not stucked anymore

--- logic/node-motion/anger.py
+++ logic/node-motion/anger.py
@@ -1,7 +1,12 @@
+import bpy
 import bge
 
 controller = bge.logic.getCurrentController()
 owner = controller.owner
 movement = controller.actuators["motion"]
 
+for sensor in controller.sensors:
+    print(sensor)
+    print("ok, showing the object now")
+    print(bpy.data.objects[sensor.name].location)
 
