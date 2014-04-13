580da7276ff9ab9db6b4fbefb540e5d0f92ebc24
vengadora <vengadoravg@gmail.com>
2014-04-13 13:55:56 -0400

    computing velocity and distance between 'brothers' like a boss

--- logic/node-motion/just-do-it/myMotion.py
+++ logic/node-motion/just-do-it/myMotion.py
@@ -1,20 +1,24 @@
+import bpy
 import bge
-import bgy
 
 controller = bge.logic.getCurrentController()
 owner = controller.owner
 movement = controller.actuators["motion"]
 
+print(":  got the signal")
 if controller.sensors["me"].positive:
     for sensor in controller.sensors:
         if sensor.name != "me" and sensor.positive:
+            print(":  i'm with", sensor.name)
             brother = bpy.data.objects[sensor.name]
-            vector[0.0, 0.0, 0.0]
+            vector = [0.0, 0.0, 0.0]
             distance = 0
             
             for i in range(3):
                 vector[i] += owner.localPosition[i] - brother.location[i]
                 distance += vector[i]**2
                 
-            distance = distance ** (0.5)
-            
+            strength = bpy.data.objects[owner.name][sensor.name]
+            distance = (distance ** (0.5)) * strength
+
+            print("done! distance: ", distance, " vector ", vector)
