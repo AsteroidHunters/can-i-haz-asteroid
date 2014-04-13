8d36b60c851bb2561c07926dc64bec493f52e091
vengadora <vengadoravg@gmail.com>
2014-04-13 15:52:58 -0400

    documented what has to be changed

--- logic/node-motion/just-do-it/myMotion.py
+++ logic/node-motion/just-do-it/myMotion.py
@@ -16,8 +16,12 @@ if controller.sensors["me"].positive:
             else:
                 print("[ACTION]:  compressing", owner.name, "and", sensor.name)
                 brother = bpy.data.objects[sensor.name]
-                vector = [0.0, 0.0, 0.0]
-                distance = 0
+                
+                #computing wires motion simulation
+                #(this is the buggy stuff)
+                vector = [0.0, 0.0, 0.0] #the force that will be applied 
+                distance = 0 #the distance between 'this and 'brother'
+                #not a big deal, i need this to compute the force applied to owner.
         
                 for i in range(3):
                     vector[i] += (owner.localPosition[i] - brother.location[i])*-1
@@ -30,5 +34,11 @@ if controller.sensors["me"].positive:
                     vector[i] += movement.force[i]
 
                 movement.force = vector
+                #end of buggy part? someone have to rethink the equations
+                #i'm using coulomb's law to calculate the motion of the nodes
+                #but it doesnt seem to be enough to emulate wires :(
+                #here is the equation: 
+                #owner.force = (brother.position - owner.position)/distance
+
                 controller.activate(movement)
                 print("done! distance: ", distance, " vector ", vector)
